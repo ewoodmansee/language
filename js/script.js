de8ee18b284262db6726fd8c29eee5ffe28e929a
Erica Woodmansee <e.woodmansee@gmail.com>
2020-10-23 08:58:06 -0700

    Continued fleshing out the structure.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,16 @@
-function fruitFunction(){
-
-	var q1 = document.quiz.q1.value;
-    var correct = 0;
+function languageFunction() {
 
 
-if (q1 == "yes") {
-    correct++;
+
+
+
+
+
+
+
+
+
+
+
+	
 }
-
-var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
-var messages = ["Great job!", "That's just okay", "You really need to do better"];
-var total;
-
-	if (correct == 0) {
-		total = 2;
-	}
-
-	if (correct > 0 && correct < 3) {
-		score = 1;
-	}
-
-	if (correct == 3) {
-		total = 0;
-    }
-
-
-	document.getElementById("after_submit").style.visibility = "visible";
-
-	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[score];
-	}
