119aeacfb6a98897b0a30e7f1510e8f2aaa1b455
Erica Woodmansee <e.woodmansee@gmail.com>
2020-10-23 10:53:47 -0700

    Abandoned jQuery attempts, found another way this works.

--- js/script.js
+++ js/script.js
@@ -1,58 +1,27 @@
-//business logic
-
-
-//user interface logic
 function languageFunction() {
 	let q1 = document.quiz.q1.value;
 	let q2 = document.quiz.q2.value;
 	let q3 = document.quiz.q3.value;
 	let q4 = document.quiz.q4.value;
 	let q5 = document.quiz.q5.value;
-		let count = 0;
 
-	if (q1 = "yes") {
-		count++;
-	}
-
-	if (q2 = "yes") {
-		count++;
-	}
-
-	if (q3 = "yes") {
-		count++;
-	}
-
-	if (q4 = "yes") {
-		count++;
-	}
-
-	if (q5 = "yes") {
-		count++;
-	}
-
-
-	if (count === 0) {
-		code = 0;
-	}
-
-	if (count === 5) {
-		code = 5;
-	}
-
-	if (> 0 && count < 5) {
-		code = 2;
-	}
-
-	if (code === 0){
-		alert("Learn JavaScript!")
-	}
-
-	if (code === 5){
-		alert("Learn C#!")
-	}
-
-	if (code === 2){
-		alert("Learn Go!")
-	}
+	if (q1 = "yes" && q2 ="yes" && q3 = "yes" && q4 = "yes" && q5 = "yes") {
+		alert("Javascript!")
+	} 
+	if (q1 = "yes" && q2 ="yes" && q3 = "yes" && q4 = "yes" && q5 = "no") {
+		alert("Javascript?")
+	} 
+	if (q1 = "yes" && q2 ="yes" && q3 = "yes" && q4 = "no" && q5 = "no") {
+		alert("C#?")
+	} 
+	if (q1 = "yes" && q2 ="yes" && q3 = "no" && q4 = "no" && q5 = "no") {
+		alert("C#.")
+	} 
+	if (q1 = "yes" && q2 ="no" && q3 = "no" && q4 = "no" && q5 = "no") {
+		alert("Ruby?")
+	} 
+	else (q1 = "no" && q2 ="no" && q3 = "no" && q4 = "no" && q5 = "no") {
+		alert("Ruby?")
+	} 
 
 }
