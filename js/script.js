253c739eee7e383704a62042c354ca8888856579
Erica Woodmansee <e.woodmansee@gmail.com>
2020-10-23 11:39:54 -0700

    Failed attempt at using jQuery; submitting only for guideline reasons.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,107 @@
-function languageFunction() {
+$(document).ready(function() {
+	$("form#quiz").submit1(function() {
+	  event.preventDefault();
+	  let question1Input = $("input#q1").val();
+
+	if (question1Input = "yes") {
+		alert("WHY")
+	  } 
+	if (question1Input = "no") {
+		alert("HELP")
+	}
+
+
+	}}
+
+/*	if (q2 = "yes") {
+		$('#q5').show();
+	  } else {
+		$('#ruby').show();
+	}
+
+	if (q3 = "yes") {
+		$('#q4').show();
+	  } else {
+		$('#javascript').show();
+	}
+
+	if (q4 = "yes") {
+		$('#ruby').show();
+	  } else {
+		$('#javascript').show();
+	}
+
+	if (q5 = "yes") {
+			$('#q4').show();
+	  } else {
+			$('#csharp').show();
+	  }
+
+	}}
+
+
+// Vintage button for below codes: <input type="button" id="button" value="What language?" onclick="languageFunction();">
+
+//My original attempt below, which does work.
+
+/* function languageFunction() {
+	let q1 = document.quiz.q1.value;
+	let q2 = document.quiz.q2.value;
+	let q3 = document.quiz.q3.value;
+	let q4 = document.quiz.q4.value;
+	let q5 = document.quiz.q5.value;
+		let count = 0;
+
+	if (q1 = "yes") {
+		count++;
+	}
+
+	if (q2 = "yes") {
+		count++;
+	}
+
+	if (q3 = "yes") {
+		count++;
+	}
+
+	if (q4 = "yes") {
+		count++;
+	}
+
+	if (q5 = "yes") {
+		count++;
+	}
+
+
+	if (count === 0) {
+		code = 0;
+	}
+
+	if (count === 5) {
+		code = 5;
+	}
+
+	if (count > 0 && count < 5) {
+		code = 2;
+	}
+
+	if (code === 0){
+		alert("Learn JavaScript!")
+	}
+
+	if (code === 5){
+		alert("Learn C#!")
+	}
+
+	if (code === 2){
+		alert("Learn Go!")
+	}
+
+}
+
+// Attempt two which worked for a time until I realized that coding out all of the possibilities would be terrible...and for some reason, whenever I try to introduce an 'else if' it promptly breaks. Current form is semi-broken as I tried to include all of the possibilities, but a version of this worked.
+
+/* function languageFunction() {
 	let q1 = document.quiz.q1.value;
 	let q2 = document.quiz.q2.value;
 	let q3 = document.quiz.q3.value;
@@ -9,19 +112,22 @@ function languageFunction() {
 		alert("Javascript!")
 	} 
 	if (q1 = "yes" && q2 ="yes" && q3 = "yes" && q4 = "yes" && q5 = "no") {
-		alert("Javascript?")
+		alert("Javascript!")
 	} 
 	if (q1 = "yes" && q2 ="yes" && q3 = "yes" && q4 = "no" && q5 = "no") {
-		alert("C#?")
+		alert("Javascript!")
 	} 
 	if (q1 = "yes" && q2 ="yes" && q3 = "no" && q4 = "no" && q5 = "no") {
-		alert("C#.")
+		alert("Javascript!")
 	} 
 	if (q1 = "yes" && q2 ="no" && q3 = "no" && q4 = "no" && q5 = "no") {
-		alert("Ruby?")
+		alert("Javascript!")
 	} 
-	else (q1 = "no" && q2 ="no" && q3 = "no" && q4 = "no" && q5 = "no") {
-		alert("Ruby?")
+	if (q1 = "no" && q2 ="no" && q3 = "no" && q4 = "no" && q5 = "no") {
+		alert("Javascript!")
 	} 
+	if {
+		alert("no!")
+	}
 
 }
